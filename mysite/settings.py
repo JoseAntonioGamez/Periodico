0310b0c062854ba2129efe4a3ad5068ab1edb293
José Antonio Gámez Granado <jose.gamez.granado.al@iespoligonosur.org>
2025-12-16 09:33:42 +0100

    Cambios env

--- mysite/settings.py
+++ mysite/settings.py
@@ -12,21 +12,27 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
+import environ
+
+
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'),True)
+env = environ.Env()
+SECRET_KEY =  env("SECRET_KEY")
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-mtt#i&%=+ig@aij!j)n+pschckvpcy3)bd@rd6u*chw$gs2bcv'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com','0.0.0.0']
+ALLOWED_HOSTS = ['127.0.0.1', 'jgamezgranado.pythonanywhere.com', '0.0.0.0']
 INTERNAL_IPS = ["127.0.0.1"]
 
 # Application definition
